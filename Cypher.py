5a0c73270dc7876244007ebdebe293b2b08a7607
Caio T Silveira <caio.tsilveira@gmail.com>
2024-03-04 13:20:50 +1300

    4/3 Commit

--- Cypher.py
+++ Cypher.py
@@ -1,4 +1,7 @@
-import  sys
+from colorama import Fore, init  # Import the colorama for colored text
+init()  # Initialize the colorama library for colored text.
+
+a = True
 def implement_caesar_cipher(message, key, decrypt=False):
    # Initialize an empty string to store the result.
    result = ""
@@ -20,17 +23,46 @@ def implement_caesar_cipher(message, key, decrypt=False):
            result += character
    return result  # Return the encrypted or decrypted result.
 
-# Prompt the user to enter the text to be encrypted
-text_to_encrypt = input("Please Enter your text/message: ")
-# Prompt the user to specify the shift length (the key).
-key = int(input("Please specify the shift length: "))
-# Check if the specified key is within a valid range (0 to 25).
-if key > 25 or key < 0:
-   # Display an error message if the key is out of range.
-   print("Your shift length should be between 0 and 25 ")
-   sys.exit()  # Exit the program if the key is invalid.
-# Encrypt the user's input using the specified key.
-encrypted_text = implement_caesar_cipher(text_to_encrypt, key)
- 
-# Display the encrypted text.
-print(text_to_encrypt,"has been encrypted as",encrypted_text)
+def encrypt(text_to_encrypt):
+    # Prompt the user to enter the text to be encrypted        
+    text_to_encrypt = input(f"{Fore.WHITE}Please Enter your text/message: ")
+    # Prompt the user to specify the shift length (the key).
+    while a == True:
+        key = int(input("Please specify the shift length: "))
+        # Check if the specified key is within a valid range (0 to 25).
+        if key > 25 or key < 0:
+            # Display an error message if the key is out of range.
+           print(f"{Fore.RED}[!] Your shift length should be between 0 and 25 ")
+        else:
+            a = False
+    # Encrypt the user's input using the specified key.
+    encrypted_text = implement_caesar_cipher(text_to_encrypt, key)
+    # Display the encrypted text.
+    print(f"{Fore.YELLOW} {text_to_encrypt} {Fore.WHITE}has been encrypted as {Fore.YELLOW}{encrypted_text}")
+
+def decrypt(text_to_decrypt):
+    # Prompt the user to enter the text to be encrypted        
+    text_to_decrypt = input(f"{Fore.WHITE}Please Enter your text/message: ")
+    # Prompt the user to specify the shift length (the key).
+    while a == True:
+        key = int(input("Please specify the shift length: "))
+        # Check if the specified key is within a valid range (0 to 25).
+        if key > 25 or key < 0:
+            # Display an error message if the key is out of range.
+           print(f"{Fore.RED}[!] Your shift length should be between 0 and 25 ")
+        else:
+            a = False
+    # Encrypt the user's input using the specified key.
+    decrypted_text = implement_caesar_cipher(text_to_decrypt, key, decrypt=True)
+    # Display the encrypted text.
+    print(f"{Fore.YELLOW} {text_to_decrypt} {Fore.WHITE}has been encrypted as {Fore.YELLOW}{decrypted_text}")
+
+print(f"{Fore.WHITE}")
+while a == True:
+    cryption = ("Would you Encpypt or Decrypt\nType 'E' or 'D'")
+    if cryption == "E":
+        encrypt
+    elif cryption == "D":
+        decrypt
+    else:
+        print(f"{Fore.RED}[!] Invalid input, please try again")
---
+++ READ_ME.md
@@ -0,0 +1,6 @@
+For full experience install colorama
+Go to Comand Prompt
+Type: py -m pip install colorama
+
+Base Caesar Cypher code taken from:
+https://thepythoncode.com/article/implement-caesar-cipher-in-python
