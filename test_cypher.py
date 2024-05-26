969ca4c3373f5f7049b878e9e1194ec2369cc486
Caio T Silveira <caio.tsilveira@gmail.com>
2024-05-26 17:52:20 +1200

    26.5 Commit 1 - Testing Stuff

---
+++ test_cypher.py
@@ -0,0 +1,75 @@
+import unittest
+import cypher_helper
+
+class TestEncryptDecrypt(unittest.TestCase):
+
+    def test_encrypt(self):
+        input = "bcd"
+        expected = 'cde'
+        r = cypher_helper.encrypt(input,1)
+        self.assertEqual(r,expected)
+    
+    def test_decrypt(self):
+        input = "cde"
+        expected = 'bcd'
+        r = cypher_helper.decrypt(input,1)
+        self.assertEqual(r,expected)
+        
+    def test_encrypt_therinas(self):
+        input = "THERINAStherinas"
+        expected = 'HTRENISAhtrenisa'
+        r = cypher_helper.encrypt(input,0)
+        self.assertEqual(r,expected)
+
+    def test_decrypt_therinas(self):
+        input = "HTRENISAhtrenisa"
+        expected = 'THERINAStherinas'
+        r = cypher_helper.decrypt(input,0)
+        self.assertEqual(r,expected)
+    
+    def test_encrypt_decrypt_num(self):
+        input = "1234567890"
+        expected = '1234567890'
+        rencrypt = cypher_helper.encrypt(input,1)
+        rdecrypt = cypher_helper.decrypt(input,1)
+        self.assertEqual(rencrypt,expected)
+        self.assertEqual(rdecrypt,expected)
+
+    def test_encrypt_decrypt_sym(self):
+        input = "~`!@#$%^&*()_-++{[}]|\:;'<,>.?/"
+        expected = "~`!@#$%^&*()_-++{[}]|\:;'<,>.?/"
+        rencrypt = cypher_helper.encrypt(input,1)
+        rdecrypt = cypher_helper.decrypt(input,1)
+        self.assertEqual(rencrypt,expected)
+        self.assertEqual(rdecrypt,expected)
+    
+    def test_encrypt_decrypt_quotes(self):
+        input = '""'
+        expected = '""'
+        rencrypt = cypher_helper.encrypt(input,1)
+        rdecrypt = cypher_helper.decrypt(input,1)
+        self.assertEqual(rencrypt,expected)
+        self.assertEqual(rdecrypt,expected)
+        
+    def test_encrypt_decrypt_space(self):
+        input = ' '
+        expected = ' '
+        rencrypt = cypher_helper.encrypt(input,1)
+        rdecrypt = cypher_helper.decrypt(input,1)
+        self.assertEqual(rencrypt,expected)
+        self.assertEqual(rdecrypt,expected)
+    
+    def test_encrypt_therinas_shifted(self):
+        input = "THERINAStherinas"
+        expected = 'IUSFOJTBiusfojtb'
+        r = cypher_helper.encrypt(input,1)
+        self.assertEqual(r,expected)
+
+    def test_decrypt_therinas_shifted(self):
+        input = "IUSFOJTBiusfojtb"
+        expected = 'THERINAStherinas'
+        r = cypher_helper.decrypt(input,1)
+        self.assertEqual(r,expected)
+
+if __name__ == '__main__':
+    unittest.main()
